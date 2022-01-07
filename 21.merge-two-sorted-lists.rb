2a502cc30af9906d0bbec0e038f92cf004f033dc
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-07 14:10:18 +0900

    update merge_two_lists

--- 21.merge-two-sorted-lists.rb
+++ 21.merge-two-sorted-lists.rb
@@ -17,7 +17,27 @@
 # @param {ListNode} list2
 # @return {ListNode}
 def merge_two_lists(list1, list2)
-    
+    head = current = ListNode.new('')
+    while (list1 || list2)
+        if list1 && list2 
+           if list1.val <= list2.val
+             current.next = list1
+             current = current.next
+              list1 = list1.next 
+            elsif list2.val < list1.val      
+             current.next = list2
+             current = current.next
+             list2 = list2.next
+           end  
+        elsif list1 && !list2   
+           current.next = list1 
+           return head.next
+        elsif list2 && !list1
+            current.next = list2
+            return head.next
+        end  
+    end   
+    return head.next
 end
 # @lc code=end
 
