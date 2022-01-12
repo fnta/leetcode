702e8b20df9cc3629c44b75c832e53532485c9d2
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-12 22:14:28 +0900

    implement-str-str

---
+++ 28.implement-str-str.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=28 lang=ruby
+#
+# [28] Implement strStr()
+#
+
+# @lc code=start
+# @param {String} haystack
+# @param {String} needle
+# @return {Integer}
+def str_str(haystack, needle)
+    
+end
+# @lc code=end
