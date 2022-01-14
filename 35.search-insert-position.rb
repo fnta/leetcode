72f597ac26202d39a7a6eee91b866d26e4db6f04
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-14 22:14:06 +0900

    search-insert-position

---
+++ 35.search-insert-position.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=35 lang=ruby
+#
+# [35] Search Insert Position
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer}
+def search_insert(nums, target)
+    
+end
+# @lc code=end
+
