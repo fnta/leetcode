6c6e1cfda87441d206d166f55f537a43edbab0e2
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-15 22:14:09 +0900

    update search-insert-position

--- 35.search-insert-position.rb
+++ 35.search-insert-position.rb
@@ -9,7 +9,18 @@
 # @param {Integer} target
 # @return {Integer}
 def search_insert(nums, target)
-    
+    if nums.include?(target)
+        return nums.index(target)
+    end
+
+    for i in 0..nums.size-2
+        if target > nums[i] && target < nums[i+1]
+            return i+1
+        end
+    end
+
+    return 0 if target < nums.min
+    return nums.size if target > nums.max
 end
 # @lc code=end
 
