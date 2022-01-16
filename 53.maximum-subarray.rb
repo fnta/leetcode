1f14374f3acef622a121eff9a30b01e5d16c6a12
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-16 20:07:28 +0900

    maximum-subarray

---
+++ 53.maximum-subarray.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=53 lang=ruby
+#
+# [53] Maximum Subarray
+#
+
+# @lc code=start
+# @param {Integer[]} nums
+# @return {Integer}
+def max_sub_array(nums)
+    
+end
+# @lc code=end
+
