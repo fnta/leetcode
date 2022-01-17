3f4b42afc7ae3037bd3abe109f66e464f59887a9
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-17 22:10:07 +0900

    update maximum-subarray

--- 53.maximum-subarray.rb
+++ 53.maximum-subarray.rb
@@ -8,7 +8,13 @@
 # @param {Integer[]} nums
 # @return {Integer}
 def max_sub_array(nums)
-    
+    max_sum = -1.0/0 # negative infinity
+    dp = []
+    nums.each_with_index do |num, i|
+      dp[i] = dp[i-1] ? [num, num + dp[i-1]].max : num
+      max_sum = [max_sum, dp[i]].max
+    end
+    max_sum  
 end
 # @lc code=end
 
