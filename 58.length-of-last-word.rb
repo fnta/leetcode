7159f58128245354606d6106b91285f750effa8c
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-18 23:08:17 +0900

    length_of_last_word

---
+++ 58.length-of-last-word.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=58 lang=ruby
+#
+# [58] Length of Last Word
+#
+
+# @lc code=start
+# @param {String} s
+# @return {Integer}
+def length_of_last_word(s)
+    
+end
+# @lc code=end
+
