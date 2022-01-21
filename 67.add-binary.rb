2ac65d02f2bd4ea3d70c4736d4eb436bc288e884
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-21 22:35:39 +0900

    add_binary

---
+++ 67.add-binary.rb
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=67 lang=ruby
+#
+# [67] Add Binary
+#
+
+# @lc code=start
+# @param {String} a
+# @param {String} b
+# @return {String}
+def add_binary(a, b)
+    
+end
+# @lc code=end
+
