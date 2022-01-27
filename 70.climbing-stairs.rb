225cf203598182405ef3628f71d42cb806893d29
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-27 17:57:43 +0900

    update Climbing_Stairs

--- 70.climbing-stairs.rb
+++ 70.climbing-stairs.rb
@@ -1,4 +1,3 @@
-#
 # @lc app=leetcode id=70 lang=ruby
 #
 # [70] Climbing Stairs
@@ -8,7 +7,7 @@
 # @param {Integer} n
 # @return {Integer}
 def climb_stairs(n)
-    
+    Hash.new { |h, k| h[k] = h[k-1] + h[k-2] }.merge({1=>1, 2=>2})[n]
 end
 # @lc code=end
 
