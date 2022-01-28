3c6c23ff4361d3754f61036494c27c27a2d511f8
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-28 21:40:14 +0900

    remove duplicates from sorted list

---
+++ 83.remove-duplicates-from-sorted-list.rb
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=83 lang=ruby
+#
+# [83] Remove Duplicates from Sorted List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def delete_duplicates(head)
+    
+end
+# @lc code=end
+
