63122c7a0e91cb0d3068d7de1ba9e2666c610d0a
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-29 21:42:41 +0900

    update remove duplicates from sorted list

--- 83.remove-duplicates-from-sorted-list.rb
+++ 83.remove-duplicates-from-sorted-list.rb
@@ -16,7 +16,17 @@
 # @param {ListNode} head
 # @return {ListNode}
 def delete_duplicates(head)
-    
+    cursor = head
+
+    while cursor and cursor.next
+      if cursor.next.val == cursor.val
+        cursor.next = cursor.next.next
+      else
+        cursor = cursor.next
+      end
+    end
+  
+    head
 end
 # @lc code=end
 
