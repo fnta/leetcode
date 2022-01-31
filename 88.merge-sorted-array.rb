4ac0ea97cbc0a78438053d0f7696effb9ccc7352
hiroshi saito <hiroshi.saitou@gmail.com>
2022-01-31 17:37:25 +0900

    Merge Sorted Array

---
+++ 88.merge-sorted-array.rb
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=88 lang=ruby
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+    
+end
+# @lc code=end
+
