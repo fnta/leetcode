a28292ecdafcf760daddfd8d326711018e05b72a
hiroshi saito <hiroshi.saitou@gmail.com>
2022-02-08 21:44:05 +0900

    add Same Tree

---
+++ 100.same-tree.rb
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=100 lang=ruby
+#
+# [100] Same Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
+def is_same_tree(p, q)
+    
+end
+# @lc code=end
+
