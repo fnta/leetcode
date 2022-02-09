5a8d590461709c9b23d4971cb719335ee549e7ba
hiroshi saito <hiroshi.saitou@gmail.com>
2022-02-09 22:14:02 +0900

    update Same Tree

--- 100.same-tree.rb
+++ 100.same-tree.rb
@@ -18,7 +18,9 @@
 # @param {TreeNode} q
 # @return {Boolean}
 def is_same_tree(p, q)
-    
+    return true if p.nil? && q.nil?
+    return false if (p.nil? || q.nil? || p.val != q.val)
+    return is_same_tree(p.left, q.left) && is_same_tree(p.right, q.right)
 end
 # @lc code=end
 
