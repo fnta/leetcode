35eaea73a5980f690921d2575867c6b0a93d523c
hiroshi saito <hiroshi.saitou@gmail.com>
2022-02-10 21:55:43 +0900

    add max_depth

---
+++ 104.maximum-depth-of-binary-tree.rb
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=104 lang=ruby
+#
+# [104] Maximum Depth of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+    
+end
+# @lc code=end
+
