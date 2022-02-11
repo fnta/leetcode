6ab065b7ad320c5073fc2c1a29c565327b9cc4af
hiroshi saito <hiroshi.saitou@gmail.com>
2022-02-11 20:33:25 +0900

    update max_depth

--- 104.maximum-depth-of-binary-tree.rb
+++ 104.maximum-depth-of-binary-tree.rb
@@ -16,6 +16,18 @@
 # end
 # @param {TreeNode} root
 # @return {Integer}
+# t - top, l - left, r - right
+t = TreeNode.new(3)
+tl = TreeNode.new(9)
+tr = TreeNode.new(20)
+trl = TreeNode.new(15)
+trr = TreeNode.new(7)
+
+t.left = tl
+t.right = tr
+  tr.left = trl
+  tr.right = trr
+
 def max_depth(root)
     
 end
