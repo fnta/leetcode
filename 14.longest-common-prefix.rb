d35e115d94f087e8350c27d58940d6519b783dee
hiroshi saito <hiroshi.saitou@gmail.com>
2022-02-18 22:23:29 +0900

    update longest common prefix

--- 14.longest-common-prefix.rb
+++ 14.longest-common-prefix.rb
@@ -7,12 +7,17 @@
 # @lc code=start
 # @param {String[]} strs
 # @return {String}
+test = ["flower","flow","flight"]
+
 def longest_common_prefix(strs)
     return strs[0] if strs.size == 1
 
     return "" if strs.empty? 
     s = ""
     base = strs[0]
+
+    p base.length-1
+
     for i in 0..base.length-1
         if strs.all?{|x| x[i] == base[i]}
             s += base[i]
@@ -22,5 +27,7 @@ def longest_common_prefix(strs)
     end
     s
 end
+
+p longest_common_prefix(test)
 # @lc code=end
 
