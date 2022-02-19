00551f55dbbb956d8ca6104eb67aedfb22aa2626
hiroshi saito <hiroshi.saitou@gmail.com>
2022-02-19 21:39:00 +0900

    update plus one

--- 66.plus-one.rb
+++ 66.plus-one.rb
@@ -7,8 +7,12 @@
 # @lc code=start
 # @param {Integer[]} digits
 # @return {Integer[]}
+test_digits = [1,2,3]
+
 def plus_one(digits)
     (digits.join.to_i + 1).to_s.chars.map(&:to_i)
 end
+
+p plus_one(test_digits)
 # @lc code=end
 
