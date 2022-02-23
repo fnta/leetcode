357a718116254f533940784523a8fe6e6d9d942d
hiroshi saito <hiroshi.saitou@gmail.com>
2022-02-23 21:32:09 +0900

    update implent str str

--- 28.implement-str-str.rb
+++ 28.implement-str-str.rb
@@ -8,7 +8,12 @@
 # @param {String} haystack
 # @param {String} needle
 # @return {Integer}
+test_haystack = "hello"
+test_needle = "ll"
+
 def str_str(haystack, needle)
     needle.empty? ? 0 : haystack.index(needle) || -1
 end
+
+p str_str(test_haystack, test_needle)
 # @lc code=end
