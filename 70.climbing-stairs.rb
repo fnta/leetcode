aa93e4fc36f5bb9d5a5c256e77244f4d290110f3
hiroshi saito <hiroshi.saitou@gmail.com>
2022-03-01 22:07:48 +0900

    update climbing stairs

--- 70.climbing-stairs.rb
+++ 70.climbing-stairs.rb
@@ -6,8 +6,12 @@
 # @lc code=start
 # @param {Integer} n
 # @return {Integer}
+test_n = 3
+
 def climb_stairs(n)
     Hash.new { |h, k| h[k] = h[k-1] + h[k-2] }.merge({1=>1, 2=>2})[n]
 end
+
+p climb_stairs(test_n)
 # @lc code=end
 
