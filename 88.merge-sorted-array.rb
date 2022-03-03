91db64780e576582b0619203b50f1e18982faafb
hiroshi saito <hiroshi.saitou@gmail.com>
2022-03-03 22:51:09 +0900

    update merge sorted array

--- 88.merge-sorted-array.rb
+++ 88.merge-sorted-array.rb
@@ -10,8 +10,14 @@
 # @param {Integer[]} nums2
 # @param {Integer} n
 # @return {Void} Do not return anything, modify nums1 in-place instead.
+test_nums1 = [1,2,3,0,0,0]
+test_m = 3
+test_nums2 = [2,5,6]
+test_n = 3
+
 def merge(nums1, m, nums2, n)
     i, j, k = m-1, n-1, m+n-1
+
     until i < 0 || j < 0
       if nums1[i] > nums2[j]
         nums1[k] = nums1[i]
@@ -26,7 +32,9 @@ def merge(nums1, m, nums2, n)
         nums1[k] = nums2[j]
         k -= 1
         j -= 1
-      end    
+    end    
 end
+
+p merge(test_nums1, 3, test_nums2, 3)
 # @lc code=end
 
