92324b2552597417a5252843e8392bfc38cd62ce
hiroshi saito <hiroshi.saitou@gmail.com>
2022-03-08 21:12:17 +0900

    add convert sorted array to binary search tree

---
+++ 108.convert-sorted-array-to-binary-search-tree.rb
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=108 lang=ruby
+#
+# [108] Convert Sorted Array to Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {Integer[]} nums
+# @return {TreeNode}
+test_nums = [-10,-3,0,5,9]
+p test_nums
+
+def sorted_array_to_bst(nums)
+    
+end
+# @lc code=end
+
