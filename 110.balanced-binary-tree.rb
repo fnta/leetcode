e5fbb09b8286b41b90ed1a9fdebb0695636af9f1
hiroshi saito <hiroshi.saitou@gmail.com>
2022-03-22 23:22:20 +0900

    update convert sorted array to binary search tree

--- 110.balanced-binary-tree.rb
+++ 110.balanced-binary-tree.rb
@@ -35,19 +35,20 @@ tn3.left = tn4
 tn3.right = tn5
 
 def is_balanced(root)
+  return depth(root) >= 0
+end
+
+def depth(root)
   if root == nil
     return 0
   end
 
-  left = is_balanced(root.left)
-  right = is_balanced(root.right)
+  left = depth(root.left)
+  right = depth(root.right)
   if(left - right).abs > 1 || left < 0 || right < 0
     return -1
   end
   return [left, right].max + 1
-end
-
-def depth(root)
 end
 
 p is_balanced(tn1)
