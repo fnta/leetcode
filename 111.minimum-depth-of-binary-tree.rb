78249f8f52546e02d2f7d8762475dfd820092d08
hiroshi saito <hiroshi.saitou@gmail.com>
2022-03-24 22:25:41 +0900

    update Minimum Depth of Binary Tree

--- 111.minimum-depth-of-binary-tree.rb
+++ 111.minimum-depth-of-binary-tree.rb
@@ -16,6 +16,15 @@
 # end
 # @param {TreeNode} root
 # @return {Integer}
+class TreeNode
+    attr_accessor :val, :left, :right
+    def initialize(val = 0, left = nil, right = nil)
+        @val = val
+        @left = left
+        @right = right
+    end
+end
+
 def min_depth(root)
     
 end
