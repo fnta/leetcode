e070d37fc110cc6e2ca42fbd59dadff86f19de77
hiroshi saito <hiroshi.saitou@gmail.com>
2022-03-26 22:05:29 +0900

    update balanced binary tree

--- 111.minimum-depth-of-binary-tree.rb
+++ 111.minimum-depth-of-binary-tree.rb
@@ -26,7 +26,18 @@ class TreeNode
 end
 
 def min_depth(root)
-    
+    def test_min_depth
+      tn1 = TreeNode.new(3)
+      tn2 = TreeNode.new(9)
+      tn3 = TreeNode.new(20)
+      tn4 = TreeNode.new(15)
+      tn5 = TreeNode.new(7)
+      tn1.left = tn2
+      tn1.right = tn3
+      tn3.left = tn4
+      tn3.right = tn5
+      assert_equal 2, min_depth(tn1)
+    end
 end
 
 require 'minitest/autorun'
