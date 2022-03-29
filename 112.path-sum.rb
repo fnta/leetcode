f493970e930946794eea1f677f8b0f1bc650712e
hiroshi saito <hiroshi.saitou@gmail.com>
2022-03-29 20:55:23 +0900

    path sum

---
+++ 112.path-sum.rb
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=112 lang=ruby
+#
+# [112] Path Sum
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @param {Integer} target_sum
+# @return {Boolean}
+def has_path_sum(root, target_sum)
+    
+end
+# @lc code=end
+
