fe022363a06ada31b370477e34a5f985189b6356
hiroshi saito <hiroshi.saitou@gmail.com>
2022-04-04 20:42:35 +0900

    add pascals triangle

---
+++ 118.pascals-triangle.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=118 lang=ruby
+#
+# [118] Pascal's Triangle
+#
+
+# @lc code=start
+# @param {Integer} num_rows
+# @return {Integer[][]}
+def generate(num_rows)
+    
+end
+# @lc code=end
+
