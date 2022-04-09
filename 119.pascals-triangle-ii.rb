3bc7c1e4406e254c4e1a367c2378796b5385ea85
hiroshi saito <hiroshi.saitou@gmail.com>
2022-04-09 20:24:38 +0900

    pascals triangle 2

---
+++ 119.pascals-triangle-ii.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=119 lang=ruby
+#
+# [119] Pascal's Triangle II
+#
+
+# @lc code=start
+# @param {Integer} row_index
+# @return {Integer[]}
+def get_row(row_index)
+    
+end
+# @lc code=end
+
