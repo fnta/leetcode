0ff927fdec971d78dbdc396c62412e84b0047973
hiroshi saito <hiroshi.saitou@gmail.com>
2022-04-10 21:39:00 +0900

    update pascals triangle 2

--- 119.pascals-triangle-ii.rb
+++ 119.pascals-triangle-ii.rb
@@ -8,7 +8,16 @@
 # @param {Integer} row_index
 # @return {Integer[]}
 def get_row(row_index)
+  result = []
     
 end
 # @lc code=end
 
+require 'minitest/autorun'
+
+class PascalsTriangle2Test < Minitest::Test
+  def test_get_row
+    assert_equal [[1,3,3,1]], get_row(3)
+    assert_equal [[1]], get_row(0)
+  end
+end
