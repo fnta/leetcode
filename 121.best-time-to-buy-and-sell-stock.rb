45875747e5151aeae4d419f0cc756979ab7f9872
hiroshi saito <hiroshi.saitou@gmail.com>
2022-04-18 21:21:11 +0900

    Add Best Time to Buy and Sell Stock

---
+++ 121.best-time-to-buy-and-sell-stock.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=121 lang=ruby
+#
+# [121] Best Time to Buy and Sell Stock
+#
+
+# @lc code=start
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+    
+end
+# @lc code=end
+
