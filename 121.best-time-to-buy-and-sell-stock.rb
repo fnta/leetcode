80a2daeb9d2db49a335f8a6e92133e2cfcc0db19
hiroshi saito <hiroshi.saitou@gmail.com>
2022-04-19 22:28:22 +0900

    Update Best Time to Buy and Sell Stock

--- 121.best-time-to-buy-and-sell-stock.rb
+++ 121.best-time-to-buy-and-sell-stock.rb
@@ -11,4 +11,12 @@ def max_profit(prices)
     
 end
 # @lc code=end
+require 'minitest/autorun'
+
+class MaxProfit < Minitest::Test
+  def test_max_profit
+    assert_equal 5, max_profit(7,1,5,3,6,4)
+    assert_equal 0, max_profit(7,6,4,3,1)
+  end
+end
 
