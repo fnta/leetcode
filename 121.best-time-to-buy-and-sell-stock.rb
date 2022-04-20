efca686bc51d2603dad5bf73f41230cfccc13824
hiroshi saito <hiroshi.saitou@gmail.com>
2022-04-20 21:11:37 +0900

    Update Best Time to Buy and Sell Stock

--- 121.best-time-to-buy-and-sell-stock.rb
+++ 121.best-time-to-buy-and-sell-stock.rb
@@ -8,15 +8,24 @@
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-    
+  ans = sum = 0
+  (1...prices.size).each do |i|
+    sum += prices[i]-prices[i-1]
+    ans = [ans, sum].max
+    sum = [sum, 0].max
+  end
+  ans
 end
 # @lc code=end
 require 'minitest/autorun'
 
 class MaxProfit < Minitest::Test
   def test_max_profit
-    assert_equal 5, max_profit(7,1,5,3,6,4)
-    assert_equal 0, max_profit(7,6,4,3,1)
+    test1 = [7,1,5,3,6,4]
+    test2 = [7,6,4,3,1]
+
+    assert_equal 5, max_profit(test1)
+    assert_equal 0, max_profit(test2)
   end
 end
 
