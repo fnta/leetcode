b3611c80b30f61732693a58de7ca05539a412c69
hiroshi saito <hiroshi.saitou@gmail.com>
2022-04-21 20:58:29 +0900

    Add Valid Palindrome

---
+++ 125.valid-palindrome.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=125 lang=ruby
+#
+# [125] Valid Palindrome
+#
+
+# @lc code=start
+# @param {String} s
+# @return {Boolean}
+def is_palindrome(s)
+    
+end
+# @lc code=end
+
