b9e1c1491f31effaf98ced712955058ae15dd7bd
hiroshi saito <hiroshi.saitou@gmail.com>
2022-05-03 20:59:59 +0900

    Update Single Number

--- 136.single-number.rb
+++ 136.single-number.rb
@@ -13,3 +13,15 @@ end
 # @lc code=end
 
 require 'minitest/autorun'
+
+class SingleNumberTest < Minitest::Test
+  def test_single_number
+    test1 = [2,2,1]
+    test2 = [4,1,2,1,2]
+    test3 = [1]
+
+    assert_equal 1, single_number(test1)
+    assert_equal 4, single_number(test2)
+    assert_equal 1, single_number(test3)
+  end
+end
