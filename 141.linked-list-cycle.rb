8af16261dd618d2ebc617d60006ff70c5eaa3442
hiroshi saito <hiroshi.saitou@gmail.com>
2022-05-11 23:42:10 +0900

    Add Linked List Cycle

---
+++ 141.linked-list-cycle.rb
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=141 lang=ruby
+#
+# [141] Linked List Cycle
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {Boolean}
+def hasCycle(head)
+    
+end
+# @lc code=end
+
