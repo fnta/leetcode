0160758b440375377172d23b936a5e9e49ca750b
hiroshi saito <hiroshi.saitou@gmail.com>
2022-05-25 22:05:29 +0900

    update Linked List Cycle

--- 141.linked-list-cycle.rb
+++ 141.linked-list-cycle.rb
@@ -59,28 +59,5 @@ class ListNode
     @next = nil
   end
 end
-
-class PathSumhTest < Minitest::Test
-  def test_has_path_sum1
-    tn1 = TreeNode.new(5)
-    tn2 = TreeNode.new(4)
-    tn3 = TreeNode.new(11)
-    tn4 = TreeNode.new(7)
-    tn5 = TreeNode.new(2)
-    tn6 = TreeNode.new(8)
-    tn7 = TreeNode.new(13)
-    tn8 = TreeNode.new(4)
-    tn9 = TreeNode.new(1)
-    tn1.left = tn2
-    tn2.left = tn3
-    tn3.left = tn4
-    tn3.right = tn5
-    tn1.right = tn6
-    tn6.left = tn7
-    tn6.right = tn8
-    tn8.right = tn9
-    assert_equal true, has_path_sum(tn1, 22)
-  end
-end
 # @lc code=end
 
