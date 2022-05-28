09e6f17292da2e355be0c091a04ee15d2362935f
hiroshi saito <hiroshi.saitou@gmail.com>
2022-05-28 22:05:29 +0900

    update Two Sum

--- 1.two-sum.rb
+++ 1.two-sum.rb
@@ -9,20 +9,8 @@
 # @param {Integer} target
 # @return {Integer[]}
 
-test_nums = [2,7,11,15]
-test_target = 9
-
 def two_sum(nums, target)
-    hash = {}
-    (0..nums.length-1).each do |i|
-        hash.store(nums[i], i)
-    end
-    (0..nums.length-1).each do |i|
-        complement = target - nums[i]
-        return [i, hash.fetch(complement)] if hash.has_key?(complement) && hash.fetch(complement) != i
-    end
 end
 
-p two_sum(test_nums, test_target)
 # @lc code=end
 
