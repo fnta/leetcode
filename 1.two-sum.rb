ec1b9d830d301eb7543a9c4aef07a74bc9f1a0c3
hiroshi saito <hiroshi.saitou@gmail.com>
2022-05-30 22:05:29 +0900

    update Two Sum

--- 1.two-sum.rb
+++ 1.two-sum.rb
@@ -13,5 +13,17 @@ def two_sum(nums, target)
 end
 
 require 'minitest/autorun'
+
+class SingleNumberTest < Minitest::Test
+    def test_single_number
+      test1 = [4,1,2,1,2]
+      test2 = [2,2,1]
+      test3 = [1]
+  
+      assert_equal 1, single_number(test1)
+      assert_equal 4, single_number(test2)
+      assert_equal 1, single_number(test3)
+    end
+  end
 # @lc code=end
 
