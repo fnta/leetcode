23aba7a4b3d8a67f3cb518b497378e68d03bc925
hiroshi saito <hiroshi.saitou@gmail.com>
2022-06-02 22:05:29 +0900

    update Two Sum

--- 1.two-sum.rb
+++ 1.two-sum.rb
@@ -16,11 +16,13 @@ require 'minitest/autorun'
 
 class TwoSumTest < Minitest::Test
     def test_two_sum
+      test_nums1 = [2,7,11,15]
+      test_target1 = 9
       test1 = [4,1,2,1,2]
       test2 = [2,2,1]
       test3 = [1]
   
-      assert_equal 1, single_number(test1)
+      assert_equal [0,1], two_sum(test_nums1, test_target1)
       assert_equal 4, single_number(test2)
       assert_equal 1, single_number(test3)
     end
