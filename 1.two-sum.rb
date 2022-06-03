72a4780c2caf9d277afeada3623c9d4f5da06982
hiroshi saito <hiroshi.saitou@gmail.com>
2022-06-03 22:05:29 +0900

    update Two Sum

--- 1.two-sum.rb
+++ 1.two-sum.rb
@@ -18,12 +18,11 @@ class TwoSumTest < Minitest::Test
     def test_two_sum
       test_nums1 = [2,7,11,15]
       test_target1 = 9
-      test1 = [4,1,2,1,2]
-      test2 = [2,2,1]
-      test3 = [1]
+      test_nums2 = [3,2,4]
+      test_target2 = 6
   
       assert_equal [0,1], two_sum(test_nums1, test_target1)
-      assert_equal 4, single_number(test2)
+      assert_equal [1,2], two_sum(test_nums2, test_target2)
       assert_equal 1, single_number(test3)
     end
   end
